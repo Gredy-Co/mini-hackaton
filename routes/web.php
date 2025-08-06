09cf55a4337cc8b68b220cace7409af79a0514d8
Gredy Corrales <greddymendoza@gmail.com>
2025-08-06 08:34:13 -0600

    Fix: usa DATABASE_URL en config/database.php

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
+Route::get('/', function () {
+    return 'Laravel funciona';
+});
+
 Route::get('/posts', [PostController::class, 'index']);
+
